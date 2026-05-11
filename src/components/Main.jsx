d23c3ffeb64e1382e67d0ae61e65132812acb895
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-10 20:07:17 -0700

    Meme Generator - Get random meme

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,6 +19,15 @@ export default function Main() {
       .then(data => setAllMemes(data.data.memes))
   }, [])
 
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length)
+    const newMemeUrl = allMemes[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      imageUrl: newMemeUrl
+    }))
+  }
+
   function handleChange(event) {
     const {value, name} = event.currentTarget
     setMeme(prevMeme => ({
@@ -49,7 +58,7 @@ export default function Main() {
             value={meme.bottomText}
           />
         </label>
-        <button>Get a new meme image</button>
+        <button onClick={getMemeImage}>Get a new meme image</button>
       </div> 
       <div className="meme">
         <img src={meme.imageUrl} />
